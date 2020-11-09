2b8c8ab5a1ceac1ea12153dd014266a1102c2a75
mgharib <61594307+mgharib@users.noreply.github.com>
2020-11-09 16:54:13 -0500

    Add files via upload

--- dataRead.py
+++ dataRead.py
@@ -2,29 +2,171 @@
 import LinkedList
 
 #initializes list of CSV files
-datfile = list()
+datfilespeed = list()
+datfiledist = list()
 
 #adds the CSV files to the list
-datfile.append('Example1.csv')
-datfile.append('Example2.csv')
-datfile.append('Example3.csv')
+datfilespeed.append('Speed2.csv')
+datfilespeed.append('Speed3.csv')
+datfilespeed.append('Speed4.csv')
+datfilespeed.append('Speed5.csv')
+
+datfiledist.append('Distance1.csv')
+datfiledist.append('Distance2.csv')
+datfiledist.append('Distance3.csv')
+datfiledist.append('Distance4.csv')
+
+#strings for regex
+speed1 = 'Counter for vehicle speed within (105-120 kph)'
+speed2 = 'Counter for vehicle speed within (120-135 kph)'
+speed3 = 'Counter for vehicle speed within (135-150 kph)'
+
+distance1 = 'Counter for FCA gap setting at (Near)'
+distance2 = 'Counter for FCA gap setting at (Medium)'
+distance3 = 'Counter for FCA gap setting at (Far)'
+distance4 = 'Counter for FCA gap setting at (Off)'
+
+#initialize errors and found
+errors = 0
+found = False
 
 #creates linked list 
 valueList = LinkedList.LinkedList()
 
 #loops through all the files
-for fh in datfile:
+for fh in datfilespeed:
 	#opens the file and reads in data
 	f = open(fh, 'r')
 
 	#goes through each line
 	for line in f:
 		#sets values equal to an array of each variable in the line
+
 		values = line.split(',')
-	#	if (values[0] == whatever): // will make sure only desired values are added
-		#adds value to linked list
-		valueList.add_first(LinkedList.Node(values[2]))
+		if (speed1 in values[0]):
+			#iterate through to see if vin exists in linked list, and if so add speed
+			for Node in valueList:
+				if (Node.vin in values[4]):
+					if (int(values[2]) > 0):
+						Node.speed1 = 1
+					found = True
+
+			if (not found) :
+				#if it isn't, then add vin to linked list and set speed to 1
+				valueList.add_first(LinkedList.Node(values[4][0:6]))
+				if (int(values[2]) > 0):
+					valueList.head.speed1 = 1
+			found = False
+
+		elif (speed2 in values[0]):
+			#iterate through to see if vin exists in linked list, and if so add speed
+			for Node in valueList:
+				if (Node.vin in values[4]):
+					if (int(values[2]) > 0):
+						Node.speed2 = 1
+					found = True
+
+			if (not found) :
+				#if it isn't, then add vin to linked list and set speed to 1
+				valueList.add_first(LinkedList.Node(values[4][0:6]))
+				if (int(values[2]) > 0):
+					valueList.head.speed2 = 1
+
+			found = False
+
+		elif (speed3 in values[0]):
+			#iterate through to see if vin exists in linked list, and if so add speed
+			for Node in valueList:
+				if (Node.vin in values[4]):
+					if (int(values[2]) > 0):
+						Node.speed3 = 1
+					found = True
+
+			if (not found) :
+				#if it isn't, then add vin to linked list and set speed to 1
+				valueList.add_first(LinkedList.Node(values[4][0:6]))
+				if (int(values[2]) > 0):
+					valueList.head.speed3 = 1
+			found = False
+
 	f.close()
+
+for fh in datfiledist:
+	#opens the file and reads in data
+	f = open(fh, 'r')
+
+	#goes through each line
+	for line in f:
+		values = line.split(',')
+		if (distance1 in values[0]):
+			
+			#iterate through to see if vin exists in linked list, and if so add speed
+			for Node in valueList:
+				if (Node.vin in values[5]):
+					if (int(values[2]) > 0):
+						Node.near = int(values[2])
+					found = True
+
+			#if it isn't, then increment the errors, as we don't have that vin in the list from speed
+			if (not found):
+				errors = errors + 1
+			found = False
+
+		elif (distance2 in values[0]):
+			#iterate through to see if vin exists in linked list, and if so add speed
+			for Node in valueList:
+				if (Node.vin in values[5]):
+					if (int(values[2]) > 0):
+						Node.medium = int(values[2])
+					found = True
+
+			#if it isn't, then increment the errors, as we don't have that vin in the list from speed
+			if (not found):
+				errors = errors + 1
+			found = False
+		
+		elif (distance3 in values[0]):
+			#iterate through to see if vin exists in linked list, and if so add speed
+			for Node in valueList:
+				if (Node.vin in values[5]):
+					if (int(values[2]) > 0):
+						Node.far = int(values[2])
+					found = True
+
+			#if it isn't, then increment the errors, as we don't have that vin in the list from speed
+			if (not found):
+				errors = errors + 1
+			found = False
+
+		elif (distance4 in values[0]):
+			#iterate through to see if vin exists in linked list, and if so add speed
+			for Node in valueList:
+				if (Node.vin in values[5]):
+					if (int(values[2]) > 0):
+						Node.off = int(values[2])
+					found = True
+
+			#if it isn't, then increment the errors, as we don't have that vin in the list from speed
+			if (not found):
+				errors = errors + 1
+			found = False
+	f.close()
+
+#set the setting to the one (near far medium or off) with the highest counter
+for Node in valueList:
+	if (Node.far > Node.near and Node.far > Node.medium and Node.far > Node.off): 
+		Node.setting = 'Far'
+	elif (Node.near > Node.far and Node.near > Node.medium and Node.near > Node.off): 
+		Node.setting = 'Near'
+	elif (Node.medium > Node.far and Node.medium > Node.near and Node.medium > Node.off): 
+		Node.setting = 'Medium'
+	elif (Node.off > Node.far and Node.off > Node.near and Node.off > Node.medium ):
+		Node.setting = 'Off'
+	else :
+		errors = errors + 1
+		valueList.remove_node(Node.vin)
 
 #prints out the linked list to make sure it is correct
 print(valueList)
+#errors are cars that have no near, medium, or far, or off setting (faulted or unknown)
+print(errors)
